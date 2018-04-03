5ece6b20ca6f98c9a0a57149f39cffc3d28ec7f9
randomforestwhitaker <ryan.tanada.0@gmail.com>
2018-04-03 17:56:29 +0000

    Rename lists, simplify isNonNegative function, remove sum method def.

--- answer_1.scala
+++ answer_1.scala
@@ -1,5 +1,5 @@
-val example_list1 = List(4,3,-9, -999, 32, 75)
-val example_list2 = List(-10, 0, 999, 3, 179, 2, 1)
+val listOfInts1 = List(4,3,-9, -999, 32, 75)
+val listOfInts2 = List(-10, 0, 999, 3, 179, 2, 1)
 
 def resolver(example_list: List[Int]): Double = {
     example_list match {
@@ -13,23 +13,10 @@ def resolver(example_list: List[Int]): Double = {
 }
 
 def isNonNegative(x: Int): Boolean = {
-    x match {
-        case a if x >= 0 => true
-        case b if x < 0 => false
-    }
+    x >= 0
 }
 
-
-// TODO: resolve type mismatch on first case (Nil vs expected Int)
-def sum(xs: List[Int]): Int = {
-    xs match {
-        case xs.isEmpty => Nil
-        case x :: tail => x + sum(tail)
-    }
-}
-
-//TODO: resolve sum helper function so average function can be tested
-def average(ls: List[Int]): Double = {
-    sum(ls) / ls.length
+def average(xs: List[Int]): Double = {
+    xs.sum / xs.length
 }
 
