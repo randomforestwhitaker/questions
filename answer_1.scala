59213027fc3e2418a5bb2ef16e1287265f3e5a32
randomforestwhitaker <ryan.tanada.0@gmail.com>
2018-04-09 22:39:58 +0000

    Fix  list shadowing in match statement by renaming to  and add example scalatest.

--- answer_1.scala
+++ answer_1.scala
@@ -6,8 +6,8 @@ def resolver(xs: List[Int]): Double = {
 def average(xs: List[Int]): Double = {
     xs match {
         case Nil => Double.NaN
-        case xs => 
-            xs.sum / xs.length
+        case ys => 
+            ys.sum / ys.length
     }
 }
 
